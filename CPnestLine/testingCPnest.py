5faa58b6085f656b120d928f89ad4d81d848d86b
phiamorton <mortonso@oregonstate.edu>
2023-06-27 16:29:29 +0200

    running the test

--- CPnestLine/testingCPnest.py
+++ CPnestLine/testingCPnest.py
@@ -22,3 +22,28 @@ class Line(raynest.model.Model):
         else:
             return -np.inf
 
+    def log_likelihood(self,x):
+        logl=  -1/2 * (np.sum((self.y - (x["a"] *self.x + x["b"]))**2) )  / (self.errory**2) + np.log(1/(np.sqrt(2*np.pi)*self.errory))
+        return logl
+
+
+
+x=np.linspace(0,5,10)
+y=np.zeros(len(x))
+a_val=2
+b_val=1
+sig_y=0.05
+from numpy import random
+for i in range(len(x)):
+    mean=a_val*x[i]+b_val
+    s = np.random.normal(mean, sig_y, 1)
+    y[i]=s
+
+#print(x,y)
+#y= b_val + a_val * x +sig_y * np.random.rand(0 ,1, x.size)
+
+mymodel= Line(x,y,sig_y)
+nest = raynest.raynest(mymodel, verbose=2, nnest=1, nensemble=1, nlive=1000, maxmcmc=5000)
+nest.run()
+    
+
